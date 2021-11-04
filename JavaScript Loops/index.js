3fab439102f2b429fe74a154bcb8b2f4abe002a7
AttiR <77799634+AttiR@users.noreply.github.com>
2021-11-04 22:21:10 +0200

    Loops1

---
+++ JavaScript Loops/index.js
@@ -0,0 +1,11 @@
+// translation of text into 'whale talk'
+let phrase = "it is the best line"
+
+// how to extract the vowrls from the string
+
+const vowels = ['a, e, i, o, u']
+
+resultArray = []
+
+// now implemnet the loop to the each word of phrase and comparing it const vowels
+
